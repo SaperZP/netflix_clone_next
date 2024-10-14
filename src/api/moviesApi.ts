301127ffd0f9c384729da63970d5a89b409de4ae
Andrew Bilous <bilous.a.work@gmail.com>
2024-10-14 19:05:56 -0400

    deploy testing

--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -4,12 +4,21 @@ const API_KEY = process.env.API_KEY;
 
 export type MovieType = "now_playing" | "popular" | "top_rated" | "upcoming";
 
-export const getMovies = async (type: MovieType) => {
-  const url = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("Failed to fetch movies");
-  const { results } = await res.json();
-  return results as Movie[];
+export const getMovies = async (type: MovieType): Promise<Movie[]> => {
+  try {
+    console.log("Fetching movies...");
+    console.log("API_KEY", !!API_KEY);
+    const url = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Failed to fetch movies: ${res.status}`);
+    }
+    const { results } = await res.json();
+    return results as Movie[];
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw new Error("Unable to fetch movies");
+  }
 };
 
 export const getVideoKey = async (movieId: number | string): Promise<string> => {
